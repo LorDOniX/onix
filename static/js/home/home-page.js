aa12bb740ac9a571ed0a714ef006100d3af0c77f
Roman Makudera <roman.makudera@firma.seznam.cz>
2015-05-12 15:37:59 +0200

    homepage - obj observe

--- static/js/home/home-page.js
+++ static/js/home/home-page.js
@@ -70,6 +70,90 @@ function(
 			$template.bindTemplate(el.getEl(), this);
 		},
 
+		ed: function() {
+			var el = document.elementFromPoint(6,316);
+
+			var event = new MouseEvent('click', {
+				'bubbles': true,
+				'cancelable': true
+			});
+
+			el.dispatchEvent(event);
+		},
+
+		_angularTest: function() {
+			var x={};
+
+			var bindWatcher = function(obj, cb) {
+				var key = "$watcher";
+
+				if (!(key in obj)) {
+					obj[key] = cb || function() {};
+				}
+			};
+
+			var bindProp = function(obj, prop) {
+				var key = "$data";
+				var key2 = "$watcher";
+
+				if (!(key in obj)) {
+					obj[key] = {};
+				}
+
+				Object.defineProperty(obj, prop, {
+					get: function() {
+						//console.log('get!');
+						return obj[key][prop];
+					},
+
+					set: function(value) {
+						//console.log('set!');
+						obj[key][prop] = value;
+						if (obj[key2] && typeof obj[key2] === "function") {
+							obj[key2](prop, value);
+						}
+					}
+				});
+			};
+
+			bindWatcher(x, function() {
+				console.log("zmena set objektu x");
+				console.log(arguments);
+			});
+			bindProp(x, "hod1");
+
+			x.hod1 = "onix";
+			console.log("hodnta je " + x.hod1); // get
+
+			setTimeout(function() {
+				x.hod1 = "roman";
+			}, 1000)
+
+
+			/*
+			var propName = 'value' 
+			var get = Function("return this['" + propName + "']")
+			var set = Function("newValue", "this['" + propName + "'] = newValue")
+			var handler = { 'get': get, 'set': set, enumerable: true, configurable: true }
+			Object.defineProperty(x, propName, handler)
+
+			var testObj = {};
+			Object.defineProperty(testObj, "hod1", {
+				get: function() {
+					console.log('get!');
+					return hod1;
+				},
+				set: function(value) {
+					console.log('set!');
+					hod1 = value;
+				}
+			});
+			*/
+			
+			//testObj.hod1 = "nastavime hodntou;"
+			//console.log(testObj.hod1); // get
+		},
+
 		// ------------------------ public ----------------------------------------
 		
 		/**
