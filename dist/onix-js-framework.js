5ec378f1f768767c6f8ca5d1028c944ce6ebf849
Roman Makudera <roman.makudera@firma.seznam.cz>
2016-04-13 10:12:18 +0200

    new modifications branch

--- dist/onix-js-framework.js
+++ dist/onix-js-framework.js
@@ -1,4 +1,259 @@
-onix = (function() {
+(function() {
+	Event = Event || window.Event;
+
+	Event.prototype.stopPropagation = Event.prototype.stopPropagation || function() {
+		this.cancelBubble = true;
+	};
+
+	Event.prototype.preventDefault = Event.prototype.preventDefault || function () {
+		this.returnValue = false;
+	};
+})();
+
+if(!Array.isArray) {
+	/**
+	 * Array.isArray dle ES5 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+	 */
+	Array.isArray = function (vArg) {
+		return Object.prototype.toString.call(vArg) === "[object Array]";
+	};
+}
+
+if (!Array.prototype.forEach) { 
+	Array.prototype.forEach = function(cb, _this) {
+	    var len = this.length;
+	    for (var i=0;i<len;i++) { 
+			if (i in this) { cb.call(_this, this[i], i, this); }
+		}
+	}
+}
+
+if (!Array.prototype.every) { 
+	Array.prototype.every = function(cb, _this) {
+	    var len = this.length;
+	    for (var i=0;i<len;i++) {
+			if (i in this && !cb.call(_this, this[i], i, this)) { return false; }
+	    }
+	    return true;
+	}
+}
+
+if (!Array.prototype.indexOf) { 
+	Array.prototype.indexOf = function(item, from) {
+	    var len = this.length;
+	    var i = from || 0;
+	    if (i < 0) { i += len; }
+	    for (;i<len;i++) {
+			if (i in this && this[i] === item) { return i; }
+	    }
+	    return -1;
+	}
+}
+
+if (!("console" in window)) {
+	var emptyFn = function() {};
+
+	window.console = {};
+
+	["log", "warn", "error", "clear", "info"].forEach(function(name) {
+		window.console[name] = emptyFn;
+	});
+}
+
+if (!Object.keys) {
+	/**
+	 * Object.keys dle ES5 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+	 */
+	Object.keys = (function () {
+		'use strict';
+
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+			hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+			dontEnums = [
+				'toString',
+				'toLocaleString',
+				'valueOf',
+				'hasOwnProperty',
+				'isPrototypeOf',
+				'propertyIsEnumerable',
+				'constructor'
+			],
+			dontEnumsLength = dontEnums.length;
+
+		return function (obj) {
+			if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+
+			var result = [], prop, i;
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+			return result;
+		};
+	}());
+}
+
+(function() {
+  if(navigator.appVersion.indexOf('MSIE 8') > 0) {
+    var _slice = Array.prototype.slice;
+    Array.prototype.slice = function() {
+      if(this instanceof Array) {
+        return _slice.apply(this, arguments);
+      } else {
+        var result = [];
+        var start = (arguments.length >= 1) ? arguments[0] : 0;
+        var end = (arguments.length >= 2) ? arguments[1] : this.length;
+        for(var i=start; i<end; i++) {
+          result.push(this[i]);
+        }
+        return result;
+      }
+    };
+  }
+})();
+
+if (!Object.create) {
+	/**
+	 * Object.create dle ES5 - https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/create
+	 */
+	Object.create = function (o) {
+		if (arguments.length > 1) { throw new Error("Object.create polyfill only accepts the first parameter"); }
+		var tmp = function() {};
+		tmp.prototype = o;
+		return new tmp();
+	};
+}
+
+if (!Function.prototype.bind) {
+	/**
+	 * ES5 Function.prototype.bind
+	 * Vrací funkci zbindovanou do zadaného kontextu.
+	 * Zbylé volitelné parametry jsou předány volání vnitřní funkce.
+	 * @param {object} thisObj Nový kontext
+	 * @returns {function}
+	 */
+	Function.prototype.bind = function(thisObj) {
+		var fn = this;
+		var args = Array.prototype.slice.call(arguments, 1);
+		return function() {
+			return fn.apply(thisObj, args.concat(Array.prototype.slice.call(arguments)));
+		}
+	}
+};
+
+if (!("addEventListener" in document)) {
+	var w = Window.prototype;
+	var h = HTMLDocument.prototype;
+	var e = Element.prototype;
+
+	w["addEventListener"] = h["addEventListener"] = e["addEventListener"] = function(eventName, listener) {
+		if (eventName == "DOMContentLoaded") {
+			document.attachEvent("onreadystatechange", function() {
+				if (document.readyState === "complete") {
+					listener();
+				}
+			});
+		}
+		else {
+			var obj = this;
+
+			this.attachEvent("on" + eventName, function() {
+				return listener.apply(obj, arguments);
+			});
+		}
+	};
+
+	w["removeEventListener"] = h["removeEventListener"] = e["removeEventListener"] = function(eventName, listener) {
+		return this.detachEvent("on" + eventName, listener);
+	};
+}
+
+if (!("classList" in document.documentElement) && window.Element) {
+	(function () {
+		var prototype = Array.prototype,
+		indexOf = prototype.indexOf,
+		slice = prototype.slice,
+		push = prototype.push,
+		splice = prototype.splice,
+		join = prototype.join;
+
+		function DOMTokenList(elm) {
+			this._element = elm;
+			if (elm.className == this._classCache) { return; }
+			this._classCache = elm.className;
+			if (!this._classCache) { return; }
+
+			var classes = this._classCache.replace(/^\s+|\s+$/g,'').split(/\s+/);
+			for (var i = 0; i < classes.length; i++) {
+				push.call(this, classes[i]);
+			}
+		}
+		window.DOMTokenList = DOMTokenList;
+
+		function setToClassName(el, classes) {
+			el.className = classes.join(" ");
+		}
+
+		DOMTokenList.prototype = {
+			add: function(token) {
+				if (this.contains(token)) { return; }
+				push.call(this, token);
+				setToClassName(this._element, slice.call(this, 0));
+			},
+			contains: function(token) {
+				return (indexOf.call(this, token) != -1);
+			},
+			item: function(index) {
+				return this[index] || null;
+			},
+			remove: function(token) {
+				var i = indexOf.call(this, token);
+				if (i == -1) { return; }
+				splice.call(this, i, 1);
+				setToClassName(this._element, slice.call(this, 0));
+			},
+			toString: function() {
+				return join.call(this, " ");
+			},
+			toggle: function(token) {
+				if (indexOf.call(this, token) == -1) {
+					this.add(token);
+					return true;
+				} else {
+					this.remove(token);
+					return false;
+				}
+			}
+		};
+
+		function defineElementGetter (obj, prop, getter) {
+			if (Object.defineProperty) {
+				Object.defineProperty(obj, prop, {
+					get: getter
+				});
+			} else {
+				obj.__defineGetter__(prop, getter);
+			}
+		}
+
+		defineElementGetter(Element.prototype, "classList", function() {
+			return new DOMTokenList(this);
+		});
+	})();
+}
+;onix = (function() {
 	/**
 	 * Module/app types
 	 * @const
@@ -158,7 +413,9 @@ onix = (function() {
 		 */
 		return function(scope, callWithNew) {
 			if (callWithNew) {
-				return new (Function.prototype.bind.apply(scope || fn, [null].concat(args)))
+				var obj = Object.create(fn.prototype);
+				fn.apply(obj, args);
+				return obj;
 			}
 			else {
 				return fn.apply(scope || fn, args);
@@ -290,32 +547,10 @@ onix = (function() {
 			// onix main run
 			$inject.bind(this._run)(this);
 
-			var $q = this.getObject("$q");
-			var _promise = this.getObject("$$promise");
-			var all = [];
-
 			// run all runs
 			runs.forEach(function(run) {
-				var runO = $inject.bind(run.param)();
-
-				// returns a promise
-				if (runO && runO instanceof _promise) {
-					all.push(runO);
-				}
-			}, this);
-
-			var $route = this.getObject("$route");
-
-			if (all.length) {
-				$q.all(all)["finally"](function() {
-					// route go
-					$route.go();
-				});
-			}
-			else {
-				// route go
-				$route.go();
-			}
+				$inject.bind(run.param)();
+			});
 		},
 
 		/**
@@ -464,8 +699,8 @@ onix = (function() {
 		info: function() {
 			console.log(
 				"Onix JS Framework\n" +
-				"Version: 2.1.0\n" +
-				"Date: 15. 7. 2015"
+				"Version: 2.1.1\n" +
+				"Date: 16. 12. 2015"
 			);
 		}
 	};
@@ -2800,18 +3035,20 @@ function(
 				});
 			};
 
+			var clickFn = function(e) {
+				removeAllOpened();
+				window.removeEventListener("click", clickFn);
+			};
+
 			removeAllOpened();
 
 			if (isOpen) {
 				// outside click
-				window.removeEventListener("click");
+				window.removeEventListener("click", clickFn);
 			}
 			else {
 				// outside click
-				window.addEventListener("click", function(e) {
-					removeAllOpened();
-					window.removeEventListener("click");
-				});
+				window.addEventListener("click", clickFn);
 
 				el.classList.add(con.OPEN_CLASS);
 			}
